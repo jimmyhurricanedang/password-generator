77d591c6bc4b15549b07dd05b10aa22969f59d55
Jimmy Dang <jimmydang12@hotmail.de>
2023-12-30 18:44:40 +0100

    initial commit

---
+++ src/test/java/de/jdhcoding/passwordgenerator/PasswordGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jdhcoding.passwordgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
